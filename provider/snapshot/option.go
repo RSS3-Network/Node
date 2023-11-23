09b2da78e51f3c1e282a6e7e19e1458cb40da50d
pseudoyu <pseudoyu@connect.hku.hk>
2023-11-23 21:44:35 +0800

    chore: add some comments in snapshot client

--- provider/snapshot/option.go
+++ provider/snapshot/option.go
@@ -1,7 +1,9 @@
 package snapshot
 
+// Option is a function that can be used to configure a [client].
 type Option func(client *client) error
 
+// WithAPIKey is an [Option] that can be used to set the X-API-KEY header for
 func WithAPIKey(apiKey string) Option {
 	return func(client *client) error {
 		client.apiKey = apiKey
