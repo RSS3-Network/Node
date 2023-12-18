fd7f6845f2b1ee1843a2cfbea5216eeb01662d4a
KallyDev <kallydev@rss3.io>
2023-12-18 19:54:19 +0800

    feat(provider/ethereum): load endpoints from environment variables

--- provider/ethereum/endpoint/endpoint.go
+++ provider/ethereum/endpoint/endpoint.go
@@ -1,6 +1,9 @@
 package endpoint
 
 import (
+	"os"
+	"strings"
+
 	"github.com/naturalselectionlabs/rss3-node/schema/filter"
 	"github.com/samber/lo"
 )
@@ -28,4 +31,32 @@ func Get(network filter.Network) ([]string, bool) {
 
 func MustGet(network filter.Network) string {
 	return lo.Must(Get(network))[0]
+}
+
+func init() {
+	// Load endpoints from the environment variables.
+	for _, environment := range os.Environ() {
+		var (
+			splits     = strings.Split(environment, "=")
+			key, value = splits[0], splits[1]
+		)
+
+		// Example of the environment variable `ENDPOINT_ETHEREUM=https://rpc.ankr.com/eth`.
+		if strings.HasPrefix(key, "ENDPOINT_") {
+			splits := strings.Split(key, "_")
+
+			if len(splits) == 2 {
+				network, err := filter.NetworkString(splits[1])
+				if err != nil {
+					continue
+				}
+
+				if _, exists := endpointsMap[network]; !exists {
+					endpointsMap[network] = make([]string, 0)
+				}
+
+				endpointsMap[network] = append([]string{value}, endpointsMap[network]...)
+			}
+		}
+	}
 }
