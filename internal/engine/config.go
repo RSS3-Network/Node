53d684681664ece80a56a33abe53a3d06bab7566
KallyDev <kallydev@rss3.io>
2023-12-19 12:29:00 +0800

    feat(engine): provide an id for config

--- internal/engine/config.go
+++ internal/engine/config.go
@@ -1,7 +1,11 @@
 package engine
 
 import (
+	"encoding/json"
+	"fmt"
+
 	"github.com/naturalselectionlabs/rss3-node/schema/filter"
+	"github.com/samber/lo"
 	"gopkg.in/yaml.v3"
 )
 
@@ -16,6 +20,22 @@ type Config struct {
 	Endpoint   string         `yaml:"endpoint" validate:"required"`
 	Worker     Name           `yaml:"worker"`
 	Parameters *Options       `yaml:"parameters"`
+}
+
+// ID returns the unique identifier of the configuration.
+func (c *Config) ID() string {
+	id := fmt.Sprintf("%s.%s", c.Network, c.Worker)
+
+	if c.Parameters != nil {
+		var buffer map[string]any
+		lo.Must0(c.Parameters.Decode(&buffer))
+
+		if buffer != nil {
+			id = fmt.Sprintf("%s.%s", id, string(lo.Must(json.Marshal(buffer))))
+		}
+	}
+
+	return id
 }
 
 type Options struct {
