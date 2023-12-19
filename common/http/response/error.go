b201d4b87823f7623d2b92d726b28dee12a655e2
polebug <polebugfly@gmail.com>
2023-12-19 20:45:54 +0800

    chore

--- common/http/response/error.go
+++ common/http/response/error.go
@@ -18,34 +18,34 @@ const (
 )
 
 type ErrorResponse struct {
-	Message string    `json:"error"`
-	Code    ErrorCode `json:"error_code"`
+	Error     string    `json:"error"`
+	ErrorCode ErrorCode `json:"error_code"`
 }
 
 func BadRequestError(c echo.Context, err error) error {
 	return c.JSON(http.StatusBadRequest, &ErrorResponse{
-		Code:    ErrorCodeBadRequest,
-		Message: fmt.Sprintf("Please check your request and try again, %s", err),
+		ErrorCode: ErrorCodeBadRequest,
+		Error:     fmt.Sprintf("Please check your request and try again, %s", err),
 	})
 }
 
 func ValidateFailedError(c echo.Context, err error) error {
 	return c.JSON(http.StatusBadRequest, &ErrorResponse{
-		Code:    ErrorCodeValidateFailed,
-		Message: fmt.Sprintf("Please check your request validation and try again, %s", err),
+		ErrorCode: ErrorCodeValidateFailed,
+		Error:     fmt.Sprintf("Please check your request validation and try again, %s", err),
 	})
 }
 
 func BadParamsError(c echo.Context, err error) error {
 	return c.JSON(http.StatusBadRequest, &ErrorResponse{
-		Code:    ErrorCodeBadParams,
-		Message: fmt.Sprintf("Please check your param combination and try again, %s", err),
+		ErrorCode: ErrorCodeBadParams,
+		Error:     fmt.Sprintf("Please check your param combination and try again, %s", err),
 	})
 }
 
 func InternalError(c echo.Context, err error) error {
 	return c.JSON(http.StatusInternalServerError, &ErrorResponse{
-		Code:    ErrorCodeInternalError,
-		Message: fmt.Sprintf("An internal error has occurred, please try again later, %s", err),
+		ErrorCode: ErrorCodeInternalError,
+		Error:     fmt.Sprintf("An internal error has occurred, please try again later, %s", err),
 	})
 }
