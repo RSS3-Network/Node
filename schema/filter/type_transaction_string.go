5fab709c742193f6deed1aacd8a3cb474f2d7d4a
KallyDev <kallydev@rss3.io>
2023-12-27 21:08:49 +0800

    chore(worker/fallback): more test cases

--- schema/filter/type_transaction_string.go
+++ schema/filter/type_transaction_string.go
@@ -9,11 +9,11 @@ import (
 	"strings"
 )
 
-const _TransactionTypeName = "transferapproval"
+const _TransactionTypeName = "transferapprovalburnmint"
 
-var _TransactionTypeIndex = [...]uint8{0, 8, 16}
+var _TransactionTypeIndex = [...]uint8{0, 8, 16, 20, 24}
 
-const _TransactionTypeLowerName = "transferapproval"
+const _TransactionTypeLowerName = "transferapprovalburnmint"
 
 func (i TransactionType) String() string {
 	i -= 1
@@ -33,20 +33,28 @@ func _TransactionTypeNoOp() {
 	var x [1]struct{}
 	_ = x[TypeTransactionTransfer-(1)]
 	_ = x[TypeTransactionApproval-(2)]
+	_ = x[TypeTransactionBurn-(3)]
+	_ = x[TypeTransactionMint-(4)]
 }
 
-var _TransactionTypeValues = []TransactionType{TypeTransactionTransfer, TypeTransactionApproval}
+var _TransactionTypeValues = []TransactionType{TypeTransactionTransfer, TypeTransactionApproval, TypeTransactionBurn, TypeTransactionMint}
 
 var _TransactionTypeNameToValueMap = map[string]TransactionType{
-	_TransactionTypeName[0:8]:       TypeTransactionTransfer,
-	_TransactionTypeLowerName[0:8]:  TypeTransactionTransfer,
-	_TransactionTypeName[8:16]:      TypeTransactionApproval,
-	_TransactionTypeLowerName[8:16]: TypeTransactionApproval,
+	_TransactionTypeName[0:8]:        TypeTransactionTransfer,
+	_TransactionTypeLowerName[0:8]:   TypeTransactionTransfer,
+	_TransactionTypeName[8:16]:       TypeTransactionApproval,
+	_TransactionTypeLowerName[8:16]:  TypeTransactionApproval,
+	_TransactionTypeName[16:20]:      TypeTransactionBurn,
+	_TransactionTypeLowerName[16:20]: TypeTransactionBurn,
+	_TransactionTypeName[20:24]:      TypeTransactionMint,
+	_TransactionTypeLowerName[20:24]: TypeTransactionMint,
 }
 
 var _TransactionTypeNames = []string{
 	_TransactionTypeName[0:8],
 	_TransactionTypeName[8:16],
+	_TransactionTypeName[16:20],
+	_TransactionTypeName[20:24],
 }
 
 // TransactionTypeString retrieves an enum value from the enum constants string name.
