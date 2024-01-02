50073dd5a735f14466b32e6cb64a9255166b513a
polebug <polebugfly@gmail.com>
2024-01-02 17:44:12 +0800

    chore

--- internal/engine/worker/contract/aavegotchi/worker_test.go
+++ internal/engine/worker/contract/aavegotchi/worker_test.go
@@ -2,7 +2,6 @@ package aavegotchi_test
 
 import (
 	"context"
-	"encoding/json"
 	"math/big"
 	"testing"
 
@@ -807,7 +806,7 @@ func TestWorker_Ethereum(t *testing.T) {
 			feed, err := instance.Transform(ctx, testcase.arguments.task)
 			testcase.wantError(t, err)
 
-			t.Log(string(lo.Must(json.MarshalIndent(feed, "", "\x20\x20"))))
+			//t.Log(string(lo.Must(json.MarshalIndent(feed, "", "\x20\x20"))))
 
 			require.Equal(t, testcase.want, feed)
 		})
