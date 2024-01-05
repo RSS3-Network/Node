3c103691435ef417e311d84a9295270223507f40
pseudoyu <pseudoyu@connect.hku.hk>
2024-01-05 12:51:33 +0800

    fix: go generate cmd err

--- provider/ethereum/contract/aavegotchi/contract_erc1155_marketplace_facet.go
+++ provider/ethereum/contract/aavegotchi/contract_erc1155_marketplace_facet.go
@@ -26,6 +26,7 @@ var (
 	_ = common.Big1
 	_ = types.BloomLookup
 	_ = event.NewSubscription
+	_ = abi.ConvertType
 )
 
 // ERC1155Listing is an auto generated low-level Go binding around an user-defined struct.
@@ -159,11 +160,11 @@ func NewERC1155MarketplaceFilterer(address common.Address, filterer bind.Contrac
 
 // bindERC1155Marketplace binds a generic wrapper to an already deployed contract.
 func bindERC1155Marketplace(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
-	parsed, err := abi.JSON(strings.NewReader(ERC1155MarketplaceABI))
+	parsed, err := ERC1155MarketplaceMetaData.GetAbi()
 	if err != nil {
 		return nil, err
 	}
-	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
+	return bind.NewBoundContract(address, *parsed, caller, transactor, filterer), nil
 }
 
 // Call invokes the (constant) contract method with params as input values and
