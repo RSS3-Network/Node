14b467193616906a36bc81d5ca10c2d51abe0f42
Thomas <wxy_000000@qq.com>
2024-01-12 15:30:26 +0800

    chore: Update worker.go to fix a typo in the IQWiki enum constant.

--- internal/engine/worker.go
+++ internal/engine/worker.go
@@ -23,7 +23,7 @@ const (
 	Optimism               // optimism
 	Aavegotchi             // aavegotchi
 	Lens                   // lens
-	IQWiki                     // iqwiki
+	IQWiki                 // iqwiki
 )
 
 type Worker interface {
