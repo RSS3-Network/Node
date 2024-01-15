d2aa2d81be1e3f474b21ab26de09bf70928616e6
Nya Candy <dev@candinya.com>
2024-01-15 13:11:43 +0800

    feat: manual write migration

---
+++ internal/database/dialer/cockroachdb/migration/20240115040941_add_ens_namehash.sql
@@ -0,0 +1,18 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS "dataset_ens_namehashs"
+(
+    "hash_hex" text NOT NULL,
+    "name"     text NOT NULL,
+
+    CONSTRAINT "pk_dataset_ens_namehashs" PRIMARY KEY ("hash_hex")
+);
+
+CREATE INDEX IF NOT EXISTS idx_ensnamehash_name ON dataset_ens_namehashs (name);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP INDEX IF EXISTS idx_ensnamehash_name;
+DROP TABLE IF EXISTS "dataset_ens_namehashs";
+-- +goose StatementEnd
