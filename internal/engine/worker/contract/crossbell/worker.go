32768efafb2ce1169b86f3ab88ad11ae68f85b1a
pseudoyu <pseudoyu@connect.hku.hk>
2024-01-22 18:23:11 +0800

    feat: add crossbell note actions

--- internal/engine/worker/contract/crossbell/worker.go
+++ internal/engine/worker/contract/crossbell/worker.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"math/big"
 	"net/http"
+	"regexp"
 	"strings"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
@@ -113,6 +114,14 @@ func (w *worker) Transform(ctx context.Context, task engine.Task) (*schema.Feed,
 			actions, err = w.transformCharacterSetHandle(ctx, ethereumTask, log)
 		case w.matchSetCharacterURI(ethereumTask, log):
 			actions, err = w.transformSetCharacterURI(ctx, ethereumTask, log)
+		case w.matchPostCreated(ethereumTask, log):
+			actions, err = w.transformPostCreated(ctx, ethereumTask, log)
+		case w.matchSetNoteURI(ethereumTask, log):
+			actions, err = w.transformSetNoteURI(ctx, ethereumTask, log)
+		case w.matchDeleteNote(ethereumTask, log):
+			actions, err = w.transformDeleteNote(ctx, ethereumTask, log)
+		case w.matchMintNote(ethereumTask, log):
+			actions, err = w.transformMintNote(ctx, ethereumTask, log)
 		default:
 			continue
 		}
@@ -155,6 +164,26 @@ func (w *worker) matchCharacterSetHandle(_ *source.Task, log *ethereum.Log) bool
 // matchCSetCharacterURI matches SetCharacterURI event.
 func (w *worker) matchSetCharacterURI(_ *source.Task, log *ethereum.Log) bool {
 	return log.Address == crossbell.AddressWeb3Entry && contract.MatchEventHashes(log.Topics[0], crossbell.EventHashSetCharacterURI)
+}
+
+// matchPostCreated matches PostCreated event.
+func (w *worker) matchPostCreated(_ *source.Task, log *ethereum.Log) bool {
+	return log.Address == crossbell.AddressWeb3Entry && contract.MatchEventHashes(log.Topics[0], crossbell.EventHashPostNote)
+}
+
+// matchSetNoteURI matches SetNoteURI event.
+func (w *worker) matchSetNoteURI(_ *source.Task, log *ethereum.Log) bool {
+	return log.Address == crossbell.AddressWeb3Entry && contract.MatchEventHashes(log.Topics[0], crossbell.EventHashSetNoteURI)
+}
+
+// matchDeleteNote matches DeleteNote event.
+func (w *worker) matchDeleteNote(_ *source.Task, log *ethereum.Log) bool {
+	return log.Address == crossbell.AddressWeb3Entry && contract.MatchEventHashes(log.Topics[0], crossbell.EventHashDeleteNote)
+}
+
+// matchMintNote matches MintNote event.
+func (w *worker) matchMintNote(_ *source.Task, log *ethereum.Log) bool {
+	return log.Address == crossbell.AddressWeb3Entry && contract.MatchEventHashes(log.Topics[0], crossbell.EventHashMintNote)
 }
 
 // transformProfileCreated transforms ProfileCreated event.
@@ -252,6 +281,113 @@ func (w *worker) transformSetCharacterURI(ctx context.Context, task *source.Task
 	}, nil
 }
 
+// transformPostCreated transforms PostCreated event.
+func (w *worker) transformPostCreated(ctx context.Context, task *source.Task, log *ethereum.Log) ([]*schema.Action, error) {
+	feedType := filter.TypeSocialPost
+
+	event, err := w.eventFilterer.ParsePostNote(log.Export())
+	if err != nil {
+		return nil, fmt.Errorf("parse post note: %w", err)
+	}
+
+	fromAddress, err := w.getOwnerOf(ctx, log.BlockNumber, event.CharacterId)
+	if err != nil {
+		return nil, err
+	}
+
+	post, linkKey, platform, err := w.buildPostMetadata(ctx, log.BlockNumber, event.CharacterId, event.NoteId, "")
+	if err != nil {
+		return nil, err
+	}
+
+	targetCharacterID, targetNoteID, err := w.getLinkingNote(ctx, log.BlockNumber, linkKey)
+	if err != nil {
+		return nil, err
+	}
+
+	if targetCharacterID != nil && targetCharacterID.Int64() != 0 {
+		feedType = filter.TypeSocialComment
+
+		post.Target, _, _, err = w.buildPostMetadata(ctx, log.BlockNumber, targetCharacterID, targetNoteID, "")
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	action := w.buildPostAction(ctx, fromAddress, *task.Transaction.To, platform, feedType, *post)
+
+	return []*schema.Action{
+		action,
+	}, nil
+}
+
+// transformSetNoteURI transforms SetNoteURI event.
+func (w *worker) transformSetNoteURI(ctx context.Context, task *source.Task, log *ethereum.Log) ([]*schema.Action, error) {
+	event, err := w.eventFilterer.ParseSetNoteUri(log.Export())
+	if err != nil {
+		return nil, fmt.Errorf("parse set note uri: %w", err)
+	}
+
+	fromAddress, err := w.getOwnerOf(ctx, log.BlockNumber, event.CharacterId)
+	if err != nil {
+		return nil, err
+	}
+
+	post, _, platform, err := w.buildPostMetadata(ctx, log.BlockNumber, event.CharacterId, event.NoteId, event.NewUri)
+	if err != nil {
+		return nil, err
+	}
+
+	action := w.buildPostAction(ctx, fromAddress, *task.Transaction.To, platform, filter.TypeSocialRevise, *post)
+
+	return []*schema.Action{
+		action,
+	}, nil
+}
+
+// transformDeleteNote transforms DeleteNote event.
+func (w *worker) transformDeleteNote(ctx context.Context, task *source.Task, log *ethereum.Log) ([]*schema.Action, error) {
+	event, err := w.eventFilterer.ParseDeleteNote(log.Export())
+	if err != nil {
+		return nil, fmt.Errorf("parse delete note: %w", err)
+	}
+
+	fromAddress, err := w.getOwnerOf(ctx, log.BlockNumber, event.CharacterId)
+	if err != nil {
+		return nil, err
+	}
+
+	post, _, platform, err := w.buildPostMetadata(ctx, log.BlockNumber, event.CharacterId, event.NoteId, "")
+	if err != nil {
+		return nil, err
+	}
+
+	action := w.buildPostAction(ctx, fromAddress, *task.Transaction.To, platform, filter.TypeSocialDelete, *post)
+
+	return []*schema.Action{
+		action,
+	}, nil
+}
+
+// transformMintNote transforms MintNote event.
+func (w *worker) transformMintNote(ctx context.Context, task *source.Task, log *ethereum.Log) ([]*schema.Action, error) {
+	event, err := w.eventFilterer.ParseMintNote(log.Export())
+	if err != nil {
+		return nil, fmt.Errorf("parse mint note: %w", err)
+	}
+
+	post, _, platform, err := w.buildPostMetadata(ctx, log.BlockNumber, event.CharacterId, event.NoteId, "")
+	if err != nil {
+		return nil, err
+	}
+
+	action := w.buildPostAction(ctx, event.To, *task.Transaction.To, platform, filter.TypeSocialMint, *post)
+
+	return []*schema.Action{
+		action,
+	}, nil
+}
+
 // buildProfileAction builds profile action.
 func (w *worker) buildProfileAction(_ context.Context, from, to common.Address, actionType filter.Type, profile metadata.SocialProfile) *schema.Action {
 	return &schema.Action{
@@ -261,6 +397,61 @@ func (w *worker) buildProfileAction(_ context.Context, from, to common.Address, 
 		Type:     actionType,
 		Metadata: profile,
 	}
+}
+
+// builPostAction builds post action.
+func (w *worker) buildPostAction(_ context.Context, from common.Address, to common.Address, platform string, actionType filter.Type, post metadata.SocialPost) *schema.Action {
+	return &schema.Action{
+		From:     from.String(),
+		To:       to.String(),
+		Platform: platform,
+		Type:     actionType,
+		Metadata: post,
+	}
+}
+
+// buildPostMetadata builds post metadata.
+func (w *worker) buildPostMetadata(ctx context.Context, blockNumber, characterID, noteID *big.Int, contentURI string) (*metadata.SocialPost, common.Hash, string, error) {
+	handle, err := w.getHandle(ctx, blockNumber, characterID)
+	if err != nil {
+		return nil, common.Hash{}, "", err
+	}
+
+	var linkKey common.Hash
+
+	if contentURI == "" {
+		contentURI, linkKey, err = w.getNote(ctx, blockNumber, characterID, noteID)
+		if err != nil {
+			return nil, common.Hash{}, "", err
+		}
+	}
+
+	content, err := w.getIPFSContent(ctx, contentURI)
+	if err != nil {
+		return nil, common.Hash{}, "", err
+	}
+
+	var note NoteContent
+	if err = json.Unmarshal(content, &note); err != nil {
+		return nil, common.Hash{}, "", fmt.Errorf("unmarshal note content: %w", err)
+	}
+
+	platform := w.getNotePlatform(ctx, note.Sources)
+
+	return &metadata.SocialPost{
+		ProfileID:     characterID.String(),
+		PublicationID: noteID.String(),
+		Handle:        w.buildProfileHandleSuffix(ctx, handle),
+		Title:         note.Title,
+		Body:          note.Content,
+		Media: lo.Map(note.Attachments, func(media NoteContentAttachment, index int) metadata.Media {
+			return metadata.Media{
+				MimeType: media.MimeType,
+				Address:  media.Address,
+			}
+		}),
+		ContentURI: contentURI,
+	}, linkKey, platform, nil
 }
 
 // buildProfileMetadata builds profile metadata.
@@ -283,14 +474,14 @@ func (w *worker) buildProfileMetadata(
 	if lo.IsEmpty(address) {
 		var err error
 
-		profile.Address, err = w.getEthereumOwnerOf(ctx, blockNumber, profileID)
+		profile.Address, err = w.getOwnerOf(ctx, blockNumber, profileID)
 		if err != nil {
 			return nil, err
 		}
 	}
 
 	if lo.IsEmpty(handle) || lo.IsEmpty(uri) {
-		profileHandle, profileURI, err := w.getEthereumProfile(ctx, blockNumber, profileID)
+		profileHandle, profileURI, err := w.getProfile(ctx, blockNumber, profileID)
 		if err != nil {
 			return nil, err
 		}
@@ -299,10 +490,10 @@ func (w *worker) buildProfileMetadata(
 		uri = lo.If(lo.IsEmpty(uri), profileURI).Else(uri)
 	}
 
-	profile.Handle = w.buildEthereumProfileHandleSuffix(ctx, profile.Handle)
+	profile.Handle = w.buildProfileHandleSuffix(ctx, profile.Handle)
 
 	if lo.IsNotEmpty(uri) {
-		content, err := w.getEthereumIPFSContent(ctx, uri)
+		content, err := w.getIPFSContent(ctx, uri)
 		if err != nil {
 			return nil, err
 		}
@@ -350,14 +541,14 @@ func (w *worker) buildCharacterProfileMetadata(
 	if lo.IsEmpty(address) {
 		var err error
 
-		profile.Address, err = w.getEthereumOwnerOf(ctx, blockNumber, characterID)
+		profile.Address, err = w.getOwnerOf(ctx, blockNumber, characterID)
 		if err != nil {
 			return nil, err
 		}
 	}
 
 	if lo.IsEmpty(handle) || lo.IsEmpty(uri) {
-		characterHandle, characterURI, err := w.getEthereumCharacter(ctx, blockNumber, characterID)
+		characterHandle, characterURI, err := w.getCharacter(ctx, blockNumber, characterID)
 		if err != nil {
 			return nil, err
 		}
@@ -367,7 +558,7 @@ func (w *worker) buildCharacterProfileMetadata(
 	}
 
 	if !lo.IsEmpty(uri) {
-		content, err := w.getEthereumIPFSContent(ctx, uri)
+		content, err := w.getIPFSContent(ctx, uri)
 		if err != nil {
 			return nil, err
 		}
@@ -385,13 +576,56 @@ func (w *worker) buildCharacterProfileMetadata(
 		profile.Name = characterURI.Name
 	}
 
-	profile.Handle = w.buildEthereumProfileHandleSuffix(ctx, profile.Handle)
+	profile.Handle = w.buildProfileHandleSuffix(ctx, profile.Handle)
 
 	return profile, nil
 }
 
-// getEthereumOwnerOf gets owner of profile.
-func (w *worker) getEthereumOwnerOf(_ context.Context, blockNumber *big.Int, profileID *big.Int) (common.Address, error) {
+// getHandle gets handle of profile.
+func (w *worker) getHandle(_ context.Context, blockNumber, characterID *big.Int) (string, error) {
+	handle, err := w.characterContract.GetHandle(&bind.CallOpts{BlockNumber: blockNumber}, characterID)
+	if err != nil {
+		return "", fmt.Errorf("get handle: %w", err)
+	}
+
+	return handle, nil
+}
+
+// getLinkingNote gets linking note of profile.
+func (w *worker) getLinkingNote(_ context.Context, blockNumber *big.Int, linkKey common.Hash) (*big.Int, *big.Int, error) {
+	linking, err := w.peripheryContract.GetLinkingNote(&bind.CallOpts{BlockNumber: blockNumber}, linkKey)
+	if err != nil {
+		return nil, nil, fmt.Errorf("get linking note: %w", err)
+	}
+
+	return linking.CharacterId, linking.NoteId, nil
+}
+
+// getNote gets note of profile.
+func (w *worker) getNote(_ context.Context, blockNumber, characterID, noteID *big.Int) (string, common.Hash, error) {
+	note, err := w.characterContract.GetNote(&bind.CallOpts{BlockNumber: blockNumber}, characterID, noteID)
+	if err != nil {
+		return "", common.Hash{}, fmt.Errorf("get note: %w", err)
+	}
+
+	return note.ContentUri, note.LinkKey, nil
+}
+
+// getNotePlatform gets note platform of profile.
+func (w *worker) getNotePlatform(_ context.Context, sources []string) string {
+	filtered := lo.Filter(sources, func(s string, _ int) bool {
+		return regexp.MustCompile(`^(x|X|Cross|cross)([a-zA-Z][a-z]*)$`).MatchString(s)
+	})
+
+	if len(filtered) == 0 {
+		return filter.PlatformCrossbell.String()
+	}
+
+	return filtered[0]
+}
+
+// getOwnerOf gets owner of profile.
+func (w *worker) getOwnerOf(_ context.Context, blockNumber *big.Int, profileID *big.Int) (common.Address, error) {
 	owner, err := w.profileContract.OwnerOf(&bind.CallOpts{BlockNumber: blockNumber}, profileID)
 	if err != nil {
 		return common.Address{}, fmt.Errorf("get owner of: %w", err)
@@ -400,8 +634,8 @@ func (w *worker) getEthereumOwnerOf(_ context.Context, blockNumber *big.Int, pro
 	return owner, nil
 }
 
-// getEthereumProfile gets profile handle and uri.
-func (w *worker) getEthereumProfile(_ context.Context, blockNumber *big.Int, profileID *big.Int) (string, string, error) {
+// getProfile gets profile handle and uri.
+func (w *worker) getProfile(_ context.Context, blockNumber *big.Int, profileID *big.Int) (string, string, error) {
 	profile, err := w.profileContract.GetProfile(&bind.CallOpts{BlockNumber: blockNumber}, profileID)
 	if err != nil {
 		return "", "", fmt.Errorf("get profile: %w", err)
@@ -410,8 +644,8 @@ func (w *worker) getEthereumProfile(_ context.Context, blockNumber *big.Int, pro
 	return profile.Handle, profile.Uri, nil
 }
 
-// getEthereumAssetImageURI gets asset image uri.
-func (w *worker) getEthereumCharacter(_ context.Context, blockNumber *big.Int, characterID *big.Int) (string, string, error) {
+// getCharacter gets asset image uri.
+func (w *worker) getCharacter(_ context.Context, blockNumber *big.Int, characterID *big.Int) (string, string, error) {
 	character, err := w.characterContract.GetCharacter(&bind.CallOpts{BlockNumber: blockNumber}, characterID)
 	if err != nil {
 		return "", "", fmt.Errorf("get character: %w", err)
@@ -420,13 +654,13 @@ func (w *worker) getEthereumCharacter(_ context.Context, blockNumber *big.Int, c
 	return character.Handle, character.Uri, nil
 }
 
-// buildEthereumProfileHandleSuffix builds profile handle suffix.
-func (w *worker) buildEthereumProfileHandleSuffix(_ context.Context, handle string) string {
+// buildProfileHandleSuffix builds profile handle suffix.
+func (w *worker) buildProfileHandleSuffix(_ context.Context, handle string) string {
 	return fmt.Sprintf("%s.csb", handle)
 }
 
-// getEthereumIPFSContent gets IPFS content.
-func (w *worker) getEthereumIPFSContent(ctx context.Context, contentURI string) (json.RawMessage, error) {
+// getIPFSContent gets IPFS content.
+func (w *worker) getIPFSContent(ctx context.Context, contentURI string) (json.RawMessage, error) {
 	_, path, err := ipfs.ParseURL(contentURI)
 	if err != nil {
 		return nil, fmt.Errorf("parse ipfs url: %w", err)
