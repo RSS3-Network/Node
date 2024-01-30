20100265bfbcaa719dab7a60580ee6875600d2bc
Nya Candy <dev@candinya.com>
2024-01-30 12:07:17 +0800

    chore: use common.Hash directly as it's already []byte
    [HashLength]byte actually

--- internal/database/dialer/cockroachdb/table/dataset_ens_namehash.go
+++ internal/database/dialer/cockroachdb/table/dataset_ens_namehash.go
@@ -8,12 +8,12 @@ import (
 var _ model.ENSNamehashTransformer = (*DatasetENSNamehash)(nil)
 
 type DatasetENSNamehash struct {
-	HashHex []byte `gorm:"column:hash_hex;primary_key"`
-	Name    string `gorm:"column:name;index:idx_ensnamehash_name"`
+	HashHex common.Hash `gorm:"column:hash_hex;primary_key"`
+	Name    string      `gorm:"column:name;index:idx_ensnamehash_name"`
 }
 
 func (d *DatasetENSNamehash) Import(ensNamehash *model.ENSNamehash) (err error) {
-	d.HashHex = ensNamehash.Hash.Bytes()
+	d.HashHex = ensNamehash.Hash
 	d.Name = ensNamehash.Name
 
 	return nil
@@ -21,7 +21,7 @@ func (d *DatasetENSNamehash) Import(ensNamehash *model.ENSNamehash) (err error) 
 
 func (d *DatasetENSNamehash) Export() (*model.ENSNamehash, error) {
 	profile := model.ENSNamehash{
-		Hash: common.BytesToHash(d.HashHex),
+		Hash: d.HashHex,
 		Name: d.Name,
 	}
 
