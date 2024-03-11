ccbfffcf7e54d117e19091d404726581c65c2196
pseudoyu <pseudoyu@connect.hku.hk>
2024-03-11 11:37:46 +0800

    fix: remove test log

--- internal/engine/worker/contract/uniswap/worker_test.go
+++ internal/engine/worker/contract/uniswap/worker_test.go
@@ -2,7 +2,6 @@ package uniswap_test
 
 import (
 	"context"
-	"encoding/json"
 	"math/big"
 	"testing"
 
@@ -1970,7 +1969,7 @@ func TestWorker_Ethereum(t *testing.T) {
 			feed, err := instance.Transform(ctx, testcase.arguments.task)
 			testcase.wantError(t, err)
 
-			t.Log(string(lo.Must(json.MarshalIndent(feed, "", "\x20\x20"))))
+			//t.Log(string(lo.Must(json.MarshalIndent(feed, "", "\x20\x20"))))
 
 			require.Equal(t, testcase.want, feed)
 		})
