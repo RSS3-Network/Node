7c7774b7dad68705ea675cba317c76737cc40154
pseudoyu <pseudoyu@connect.hku.hk>
2024-04-18 16:56:07 +0800

    feat(worker/curve): support curve transactions (#200)

---
+++ internal/engine/worker/contract/curve/pool/schema.go
@@ -0,0 +1,35 @@
+package pool
+
+import (
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/rss3-network/protocol-go/schema/filter"
+)
+
+type ContractType string
+
+// Contract types.
+const (
+	ContractTypePool  ContractType = "pool"
+	ContractTypeToken ContractType = "token"
+	ContractTypeGauge ContractType = "gauge"
+)
+
+// Response is a curve pool.
+type Response[T any] struct {
+	Success   bool  `json:"success"`
+	Data      T     `json:"data"`
+	Timestamp int64 `json:"generatedTimeMs"`
+}
+
+type GetPoolData struct {
+	PoolData []Pool `json:"poolData"`
+}
+
+// Pool is a curve pool.
+type Pool struct {
+	Network                       filter.Network `json:"-"`
+	Name                          string         `json:"name"`
+	Address                       common.Address `json:"address"`
+	LiquidityProviderTokenAddress common.Address `json:"lpTokenAddress"`
+	GaugeAddress                  common.Address `json:"gaugeAddress"`
+}
