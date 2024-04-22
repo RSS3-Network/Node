47c5ae97bd31d4e51c10d496e9e1068dd8496f0a
pseudoyu <pseudoyu@connect.hku.hk>
2024-04-22 16:09:21 +0800

    chore: add commengts of new worker interface

--- internal/engine/worker.go
+++ internal/engine/worker.go
@@ -17,7 +17,10 @@ type Worker interface {
 	Tags() []tag.Tag
 	// Types the possible `type` of the worker, displayed in the final Activity response.
 	Types() []schema.Type
+	// Filter the SourceFilter of the worker(network, state, start logics, etc.).
 	Filter() SourceFilter
+	// Match the matching logic of worker and returns if the task is matched
 	Match(ctx context.Context, task Task) (bool, error)
+	// Transform the core logic of the worker and returns the Activity.
 	Transform(ctx context.Context, task Task) (*activityx.Activity, error)
 }
