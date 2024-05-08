12c6f9700c5d2916fbea15544bce8333753aeafc
KallyDev <kallydev@rss3.io>
2024-05-08 16:07:34 +0800

    fix(source/ethereum): remove invalid transaction fee field for arbitrum (#260)

--- internal/engine/source/ethereum/task.go
+++ internal/engine/source/ethereum/task.go
@@ -165,12 +165,7 @@ func (t Task) buildFeeArbitrumNitro() (*big.Int, error) {
 		types.LegacyTxType,
 		types.AccessListTxType,
 		types.DynamicFeeTxType:
-		fee, err := t.buildFeeDefault()
-		if err != nil {
-			return nil, err
-		}
-
-		return new(big.Int).Add(fee, t.Receipt.L1Fee), nil
+		return t.buildFeeDefault()
 	case // The transaction fee is `effectiveGasPrice` * `gasUsed`.
 		ethereum.TransactionTypeArbitrumContract,
 		ethereum.TransactionTypeArbitrumUnsigned,
