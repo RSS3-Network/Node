5c8da853d412bd1f3802f58161cf2fd659d52b34
brucexc <brucexc@rss3.io>
2024-05-11 14:58:29 -0400

    chore: add test

---
+++ internal/node/component/decentralized/handler_workers_test.go
@@ -0,0 +1,43 @@
+package decentralized
+
+import (
+	"testing"
+
+	"github.com/rss3-network/node/schema/worker"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDetermineFinalStatus(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name     string
+		statuses []worker.Status
+		expected worker.Status
+	}{
+		{
+			name:     "AllStatusesReady",
+			statuses: []worker.Status{worker.StatusReady, worker.StatusReady, worker.StatusReady},
+			expected: worker.StatusReady,
+		},
+		{
+			name:     "AtLeastOneStatusIndexing",
+			statuses: []worker.Status{worker.StatusReady, worker.StatusIndexing, worker.StatusReady},
+			expected: worker.StatusIndexing,
+		},
+		{
+			name:     "AtLeastOneStatusUnhealthy",
+			statuses: []worker.Status{worker.StatusReady, worker.StatusUnhealthy, worker.StatusReady},
+			expected: worker.StatusUnhealthy,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			result := determineFinalStatus(tc.statuses)
+			assert.Equal(t, tc.expected, result)
+		})
+	}
+}
