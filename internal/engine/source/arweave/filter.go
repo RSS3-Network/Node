23e0a5426d07931ae10b73931fb9c4437f918fa7
KallyDev <kallydev@rss3.io>
2024-06-05 15:47:46 +0800

    feat(source/arweave): add irys graphql query support (#373)

--- internal/engine/source/arweave/filter.go
+++ internal/engine/source/arweave/filter.go
@@ -7,6 +7,12 @@ import (
 var _ engine.DataSourceFilter = (*Filter)(nil)
 
 type Filter struct {
-	OwnerAddresses  []string `yaml:"owner_addresses"`
+	OwnerAddresses []string `yaml:"owner_addresses"`
+
+	// BundlrOnly is a tag, indicating that only transactions of Bundlr nodes are pulled.
+	BundlrOnly bool `yaml:"bundlr_only"`
+
+	// BundlrAddresses is a list of addresses that are considered Bundlr nodes.
+	// If BundlrOnly is true, the field needs to be ignored.
 	BundlrAddresses []string `yaml:"bundlr_addresses"`
 }
