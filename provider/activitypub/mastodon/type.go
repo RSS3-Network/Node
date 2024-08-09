316d7513a3c6c50da0d2e440f2d6eb91a92bd122
FrankLi123 <zoli@ucsd.edu>
2024-08-10 02:26:37 +0800

    fix: modify go-generate commands and add the type_emssage file

--- provider/activitypub/mastodon/type.go
+++ provider/activitypub/mastodon/type.go
@@ -1,6 +1,6 @@
 package mastodon
 
-//go:generate go run --mod=mod github.com/dmarkham/enumer --values --type=MessageType --output mastodon_message_type.go --transform=snake-upper
+//go:generate go run --mod=mod github.com/dmarkham/enumer --values --type=MessageType --output type_message.go --transform=snake-upper
 type MessageType int
 
 const (
