d415f1aec82ab0f8754cdfdff8b824492561b12d
pseudoyu <pseudoyu@connect.hku.hk>
2024-08-27 18:06:39 +0800

    feat: add get workers info api

--- internal/node/component/info/component.go
+++ internal/node/component/info/component.go
@@ -49,6 +49,7 @@ func NewComponent(_ context.Context, apiServer *echo.Echo, config *config.File, 
 	apiServer.GET("/version", c.GetVersion)
 	apiServer.GET("/activity_count", c.GetActivityCount)
 	apiServer.GET("/workers_status", c.GetWorkersStatus)
+	apiServer.GET("/workers_info", c.GetWorkersInfo)
 
 	networks := apiServer.Group("/networks")
 
--- internal/node/component/info/handler_worker.go
+++ internal/node/component/info/handler_worker.go
@@ -38,6 +38,17 @@ type WorkerInfo struct {
 	monitor.WorkerProgress
 }
 
+type WorkerDetailResponse struct {
+	Data []WorkerDetail `json:"data"`
+}
+
+type WorkerDetail struct {
+	Type     string `json:"type"`
+	Name     string `json:"name"`
+	Platform string `json:"platform"`
+	IconURL  string `json:"icon_url"`
+}
+
 // GetWorkersStatus returns the status of all workers.
 func (c *Component) GetWorkersStatus(ctx echo.Context) error {
 	go c.CollectTrace(ctx.Request().Context(), ctx.Request().RequestURI, "status")
@@ -70,6 +81,36 @@ func (c *Component) GetWorkersStatus(ctx echo.Context) error {
 	}
 
 	return ctx.JSON(http.StatusOK, response)
+}
+
+// GetWorkersInfo returns the worker info.
+func (c *Component) GetWorkersInfo(ctx echo.Context) error {
+	workers := make([]WorkerDetail, 0, len(decentralized.WorkerValues())+len(rss.WorkerValues()))
+
+	// Aggregate decentralized workers
+	for _, w := range decentralized.WorkerValues() {
+		platform := decentralized.ToPlatformMap[w]
+		workers = append(workers, WorkerDetail{
+			Name:     w.Name(),
+			Platform: platform.String(),
+			Type:     w.Component(),
+			IconURL:  decentralized.ToIconURLMap[w],
+		})
+	}
+
+	// Aggregate RSS workers
+	for _, w := range rss.WorkerValues() {
+		workers = append(workers, WorkerDetail{
+			Name:     w.Name(),
+			Platform: "RSSHub",
+			Type:     w.Component(),
+			IconURL:  rss.ToIconURLMap[w],
+		})
+	}
+
+	return ctx.JSON(http.StatusOK, WorkerDetailResponse{
+		Data: workers,
+	})
 }
 
 // fetchAllWorkerInfo fetches the status of all workers concurrently.
@@ -139,13 +180,13 @@ func (c *Component) fetchWorkerInfo(ctx context.Context, module *config.Module) 
 		},
 	}
 
-	switch module.Network.Source() {
-	case network.EthereumSource, network.ArweaveSource, network.FarcasterSource:
-		workerInfo.Platform = decentralized.ToPlatformMap[module.Worker.(decentralized.Worker)]
-		workerInfo.Tags = decentralized.ToTagsMap[module.Worker.(decentralized.Worker)]
+	switch w := module.Worker.(type) {
+	case decentralized.Worker:
+		workerInfo.Platform = decentralized.ToPlatformMap[w]
+		workerInfo.Tags = decentralized.ToTagsMap[w]
 
 		// Handle special tags for decentralized core workers.
-		if module.Worker == decentralized.Core {
+		if w == decentralized.Core {
 			switch module.Network {
 			case network.Farcaster:
 				workerInfo.Tags = []tag.Tag{tag.Social}
@@ -153,11 +194,10 @@ func (c *Component) fetchWorkerInfo(ctx context.Context, module *config.Module) 
 				workerInfo.Tags = []tag.Tag{tag.Transaction}
 			case network.VSL:
 				workerInfo.Tags = append(workerInfo.Tags, tag.Exchange)
-			default:
 			}
 		}
-	case network.RSSSource:
-		workerInfo.Tags = rss.ToTagsMap[module.Worker.(rss.Worker)]
+	case rss.Worker:
+		workerInfo.Tags = rss.ToTagsMap[w]
 	}
 
 	return workerInfo
