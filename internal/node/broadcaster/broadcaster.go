2e83fd276354301c55e2ae33fb07d0905630e5f5
polebug <polebugfly@gmail.com>
2024-08-29 12:24:33 +0800

    chore(broadcaster): add default type for node register (#496)

--- internal/node/broadcaster/broadcaster.go
+++ internal/node/broadcaster/broadcaster.go
@@ -22,6 +22,7 @@ func (b *Broadcaster) Register(ctx context.Context) error {
 		AccessToken: b.config.Discovery.Server.AccessToken,
 		Stream:      b.config.Stream,
 		Config:      b.config.Component,
+		Type:        "production",
 	}
 
 	var response any
