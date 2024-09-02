bdb17f5aef3f672b4e4c3dbdbf5ec28c9699a657
pseudoyu <pseudoyu@connect.hku.hk>
2024-09-02 12:10:06 +0800

    chore: remove /assets api

--- internal/node/component/info/component.go
+++ internal/node/component/info/component.go
@@ -49,7 +49,6 @@ func NewComponent(_ context.Context, apiServer *echo.Echo, config *config.File, 
 	apiServer.GET("/version", c.GetVersion)
 	apiServer.GET("/activity_count", c.GetActivityCount)
 	apiServer.GET("/workers_status", c.GetWorkersStatus)
-	apiServer.GET("/assets", c.GetNodeAssets)
 
 	networks := apiServer.Group("/networks")
 
