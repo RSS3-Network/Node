7c5da21e3a0947b888514abe39ea3f81e7e0aea9
frank <zcl@rss3.io>
2024-09-12 18:13:22 +0800

    feat: add a test case for batch offer match finalizations

--- internal/engine/worker/decentralized/contract/polymarket/worker_test.go
+++ internal/engine/worker/decentralized/contract/polymarket/worker_test.go
@@ -38,7 +38,7 @@ func TestWorker_Polymarket(t *testing.T) {
 		wantError require.ErrorAssertionFunc
 	}{
 		{
-			name: "Test Offer Match Finalization",
+			name: "Test One Offer Match Finalization(Generate Buy & Sell Actions)",
 			arguments: struct {
 				task   *source.Task
 				config *config.Module
@@ -164,6 +164,194 @@ func TestWorker_Polymarket(t *testing.T) {
 					}},
 				Status:    true,
 				Timestamp: 1726122849,
+			},
+			wantError: require.NoError,
+		},
+
+		{
+			name: "Test Batch Offer Match Finalization",
+			arguments: struct {
+				task   *source.Task
+				config *config.Module
+			}{
+				task: &source.Task{
+					Network: network.Polygon,
+					ChainID: 137,
+					Header: &ethereum.Header{
+						Hash:         common.HexToHash("0xb511b8794659ed5b89526356f7e6f001952fff7865685242fb377412374eb6a5"),
+						ParentHash:   common.HexToHash("0x2d68e2d0777fc2bcb1008a67ff79ab69ab288ea4639145a96f27b3adffa3e7be"),
+						UncleHash:    common.HexToHash("0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347"),
+						Coinbase:     common.HexToAddress("0xa8B52F02108AA5F4B675bDcC973760022D7C6020"),
+						Number:       lo.Must(new(big.Int).SetString("61729697", 0)),
+						GasLimit:     30000000,
+						GasUsed:      22240185,
+						Timestamp:    1726134259,
+						BaseFee:      lo.Must(new(big.Int).SetString("51", 0)),
+						Transactions: nil,
+					},
+					Transaction: &ethereum.Transaction{
+						BlockHash: common.HexToHash("0xa475a7f70346c3cf1c5f5496fe89963da6cedbbd2cd7abe63922a60a60842669"),
+						From:      common.HexToAddress("0x769598dc61E5fB3D0DB4Ee969B4a10EeF564de7A"),
+						Gas:       900000,
+						GasPrice:  lo.Must(new(big.Int).SetString("106500000051", 10)),
+						Hash:      common.HexToHash("0xa475a7f70346c3cf1c5f5496fe89963da6cedbbd2cd7abe63922a60a60842669"),
+						Input:     hexutil.MustDecode("0xd2539b3700000000000000000000000000000000000000000000000000000000000000a000000000000000000000000000000000000000000000000000000000000002c000000000000000000000000000000000000000000000000000000000012af09800000000000000000000000000000000000000000000000000000000000005200000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001004f4347a7000000000000000000000000c2d59964a61981bf1d040e14bb365974f507c327000000000000000000000000a01c85c1c89d15913c5e85a72632949403633b25000000000000000000000000000000000000000000000000000000000000000061ea30b65cd4a9399f92c53e91586575a6d8135943ef22bb4889b6b01a77086b00000000000000000000000000000000000000000000000000000000012af09800000000000000000000000000000000000000000000000000000000012c23f00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000001a00000000000000000000000000000000000000000000000000000000000000041d1954076951192ba328c28b7d28908d671cc2b968d0654474a0acb41bcc6c60b574dfd6f07bba009156188fdbc07c7690491b83fd91135704feeca083801b1711b0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000ac4c7c87020000000000000000000000006b7ec4ba079c0a258435ea36025f6190cd4245620000000000000000000000006e5fa2f2b3268c32966d0683a0a6fbd87f08085500000000000000000000000000000000000000000000000000000000000000001a1ad53feeb99f71ac2cb633db04714334493bbf2d6664a99f2a2f9af2ff736900000000000000000000000000000000000000000000000000000000001e8480000000000000000000000000000000000000000000000000000000001dcd65000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000001a000000000000000000000000000000000000000000000000000000000000000418e7acf53e3e1959768bcc3d55f1469312d00ebe6e75b964b01c5fac8550701201d5e4c042283049385c29c71774545976b85736abcd906d35253be4c1c5cbfad1c0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000013358"),
+						To:        lo.ToPtr(common.HexToAddress("0x56C79347e95530c01A2FC76E732f9566dA16E113")),
+						Value:     lo.Must(new(big.Int).SetString("0", 0)),
+						Type:      2,
+						ChainID:   lo.Must(new(big.Int).SetString("137", 0)),
+					},
+					Receipt: &ethereum.Receipt{
+						BlockHash:         common.HexToHash("0xb511b8794659ed5b89526356f7e6f001952fff7865685242fb377412374eb6a5"),
+						BlockNumber:       lo.Must(new(big.Int).SetString("61729697", 0)),
+						ContractAddress:   nil,
+						CumulativeGasUsed: 1886167,
+						EffectiveGasPrice: hexutil.MustDecodeBig("0x18cbe50933"),
+						GasUsed:           360318,
+
+						Logs: []*ethereum.Log{{
+							Address: common.HexToAddress("0x4bFb41d5B3570DeFd03C39a9A4D8dE6Bd8B8982E"),
+							Topics: []common.Hash{
+								common.HexToHash("0xd0a08e8c493f9c94f29311604c9de1b4e8c8d4c06bd0c789af57f2d65bfec0f6"),
+								common.HexToHash("0x4dc37aace70bd18c7e9feefc60bc4c49bec9d8a438905a7b4025ed3743a17464"),
+								common.HexToHash("0x0000000000000000000000006b7ec4ba079c0a258435ea36025f6190cd424562"),
+								common.HexToHash("0x000000000000000000000000c2d59964a61981bf1d040e14bb365974f507c327"),
+							},
+							Data:            hexutil.MustDecode("0x00000000000000000000000000000000000000000000000000000000000000001a1ad53feeb99f71ac2cb633db04714334493bbf2d6664a99f2a2f9af2ff7369000000000000000000000000000000000000000000000000000000000001335800000000000000000000000000000000000000000000000000000000012c23f00000000000000000000000000000000000000000000000000000000000000000"),
+							BlockNumber:     lo.Must(new(big.Int).SetString("61729697", 0)),
+							TransactionHash: common.HexToHash("0xa475a7f70346c3cf1c5f5496fe89963da6cedbbd2cd7abe63922a60a60842669"),
+							Index:           74,
+							Removed:         false,
+						}, {
+							Address: common.HexToAddress("0x4bFb41d5B3570DeFd03C39a9A4D8dE6Bd8B8982E"),
+							Topics: []common.Hash{
+								common.HexToHash("0xd0a08e8c493f9c94f29311604c9de1b4e8c8d4c06bd0c789af57f2d65bfec0f6"),
+								common.HexToHash("0x90480b747515144eb13d1b8da9add38b368830d94785cd64a8521843159ad458"),
+								common.HexToHash("0x000000000000000000000000c2d59964a61981bf1d040e14bb365974f507c327"),
+								common.HexToHash("0x0000000000000000000000004bfb41d5b3570defd03c39a9a4d8de6bd8b8982e"),
+							},
+							Data:            hexutil.MustDecode("0x000000000000000000000000000000000000000000000000000000000000000061ea30b65cd4a9399f92c53e91586575a6d8135943ef22bb4889b6b01a77086b00000000000000000000000000000000000000000000000000000000012af09800000000000000000000000000000000000000000000000000000000012c23f00000000000000000000000000000000000000000000000000000000000000000"),
+							BlockNumber:     lo.Must(new(big.Int).SetString("61729697", 0)),
+							TransactionHash: common.HexToHash("0xa475a7f70346c3cf1c5f5496fe89963da6cedbbd2cd7abe63922a60a60842669"),
+							Index:           76,
+							Removed:         false,
+						}},
+						Status:           1,
+						TransactionHash:  common.HexToHash("0xa475a7f70346c3cf1c5f5496fe89963da6cedbbd2cd7abe63922a60a60842669"),
+						TransactionIndex: 3,
+					},
+				},
+				config: &config.Module{
+					Network: network.Polygon,
+					Endpoint: config.Endpoint{
+						URL: endpoint.MustGet(network.Polygon),
+					},
+				},
+			},
+			want: &activityx.Activity{
+
+				ID:       "0xa475a7f70346c3cf1c5f5496fe89963da6cedbbd2cd7abe63922a60a60842669",
+				Network:  network.Polygon,
+				Index:    3,
+				From:     "0x769598dc61E5fB3D0DB4Ee969B4a10EeF564de7A",
+				To:       "0x56C79347e95530c01A2FC76E732f9566dA16E113",
+				Type:     typex.CollectibleTrade,
+				Platform: workerx.PlatformPolymarket.String(),
+				Fee: &activityx.Fee{
+					Amount:  lo.Must(decimal.NewFromString("38373867018376218")),
+					Decimal: 18,
+				},
+				Calldata: &activityx.Calldata{
+					FunctionHash: "0xd2539b37",
+				},
+				Actions: []*activityx.Action{
+					{
+						Type:     typex.CollectibleTrade,
+						Platform: workerx.PlatformPolymarket.String(),
+						From:     "0xc2D59964A61981bf1D040E14bB365974f507C327",
+						To:       "0x6b7Ec4ba079c0a258435Ea36025f6190cD424562",
+						Metadata: metadata.CollectibleTrade{
+							Action: metadata.ActionCollectibleTradeBuy,
+							Token: metadata.Token{
+								Address:  lo.ToPtr("0x4D97DCd97eC945f40cF65F87097ACe5EA0476045"),
+								ID:       lo.ToPtr(lo.Must(decimal.NewFromString("11807543882438356730428584319959237816456493548031271396061607499515283075945"))),
+								Value:    lo.ToPtr(decimal.NewFromInt(19670000)),
+								Standard: metadata.StandardERC1155,
+							},
+							Cost: &metadata.Token{
+								Value:    lo.ToPtr(decimal.NewFromInt(78680)),
+								Name:     "Polygon",
+								Symbol:   "MATIC",
+								Decimals: 18,
+							},
+						},
+					},
+					{
+						Type:     typex.CollectibleTrade,
+						Platform: workerx.PlatformPolymarket.String(),
+						From:     "0x6b7Ec4ba079c0a258435Ea36025f6190cD424562",
+						To:       "0xc2D59964A61981bf1D040E14bB365974f507C327",
+						Metadata: metadata.CollectibleTrade{
+							Action: metadata.ActionCollectibleTradeSell,
+							Token: metadata.Token{
+								Address:  lo.ToPtr("0x4D97DCd97eC945f40cF65F87097ACe5EA0476045"),
+								ID:       lo.ToPtr(lo.Must(decimal.NewFromString("11807543882438356730428584319959237816456493548031271396061607499515283075945"))),
+								Value:    lo.ToPtr(decimal.NewFromInt(19670000)),
+								Standard: metadata.StandardERC1155,
+							},
+							Cost: &metadata.Token{
+								Value:    lo.ToPtr(decimal.NewFromInt(78680)),
+								Name:     "Polygon",
+								Symbol:   "MATIC",
+								Decimals: 18,
+							},
+						},
+					},
+					{
+						Type:     typex.CollectibleTrade,
+						Platform: workerx.PlatformPolymarket.String(),
+						From:     "0x4bFb41d5B3570DeFd03C39a9A4D8dE6Bd8B8982E",
+						To:       "0xc2D59964A61981bf1D040E14bB365974f507C327",
+						Metadata: metadata.CollectibleTrade{
+							Action: metadata.ActionCollectibleTradeBuy,
+							Token: metadata.Token{
+								Address:  lo.ToPtr("0x4D97DCd97eC945f40cF65F87097ACe5EA0476045"),
+								ID:       lo.ToPtr(lo.Must(decimal.NewFromString("44288124726046135483095464909806845540163757513494361566988300031502732167275"))),
+								Value:    lo.ToPtr(decimal.NewFromInt(19670000)),
+								Standard: metadata.StandardERC1155,
+							},
+							Cost: &metadata.Token{
+								Value:    lo.ToPtr(decimal.NewFromInt(19591320)),
+								Name:     "Polygon",
+								Symbol:   "MATIC",
+								Decimals: 18,
+							},
+						},
+					},
+					{
+						Type:     typex.CollectibleTrade,
+						Platform: workerx.PlatformPolymarket.String(),
+						From:     "0xc2D59964A61981bf1D040E14bB365974f507C327",
+						To:       "0x4bFb41d5B3570DeFd03C39a9A4D8dE6Bd8B8982E",
+						Metadata: metadata.CollectibleTrade{
+							Action: metadata.ActionCollectibleTradeSell,
+							Token: metadata.Token{
+								Address:  lo.ToPtr("0x4D97DCd97eC945f40cF65F87097ACe5EA0476045"),
+								ID:       lo.ToPtr(lo.Must(decimal.NewFromString("44288124726046135483095464909806845540163757513494361566988300031502732167275"))),
+								Value:    lo.ToPtr(decimal.NewFromInt(19670000)),
+								Standard: metadata.StandardERC1155,
+							},
+							Cost: &metadata.Token{
+								Value:    lo.ToPtr(decimal.NewFromInt(19591320)),
+								Name:     "Polygon",
+								Symbol:   "MATIC",
+								Decimals: 18,
+							},
+						},
+					},
+				},
+				Status:    true,
+				Timestamp: 1726134259,
 			},
 			wantError: require.NoError,
 		},
