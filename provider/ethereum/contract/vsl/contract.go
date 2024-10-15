a9644ae66c16c19858ebc3a9b9515beaeacb5785
pseudoyu <pseudoyu@connect.hku.hk>
2024-10-15 15:04:52 +0800

    chore: update network params contract address

--- provider/ethereum/contract/vsl/contract.go
+++ provider/ethereum/contract/vsl/contract.go
@@ -27,8 +27,8 @@ var (
 	AddressL1OptimismPortal = common.HexToAddress("0x6A12432491bbbE8d3babf75F759766774C778Db4")
 
 	AddressNetworkParams = map[int64]common.Address{
-		ChainIDMainnet: common.HexToAddress("0x15176Aabdc4836c38947a67313d209204051C502"),
-		ChainIDTestnet: common.HexToAddress("0x5d768cAef86d3DA8eC6009eE4B3d9b7Fe26A43CB"),
+		ChainIDMainnet: common.HexToAddress("0x855a497619E7002430771e5886660F42A2A5Dc5e"),
+		ChainIDTestnet: common.HexToAddress("0x4d0de2CF4d8996f75466BE28Cc85306439Fb46F8"),
 	}
 
 	AddressSettlement = map[int64]common.Address{
