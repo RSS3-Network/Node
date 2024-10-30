45738b443ab0a5f39e2ca4beaa1e32dccf37b954
Henry Wang <henry@rss3.io>
2024-10-30 15:54:42 +0000

    feat: support HEAD for all GET methods & add more validations for config.yaml (#619)

---
+++ config/validator.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/samber/lo"
+)
+
+// HasOneWorker Check if there is at least one worker deployed
+func HasOneWorker(config *File) error {
+	if (CalculateWorkerCount(config)) == 0 {
+		return fmt.Errorf("at least one worker must be deployed")
+	}
+
+	return nil
+}
+
+// IsRSSComponentOnly Check if the configuration contains an RSS component only
+func IsRSSComponentOnly(config *File) bool {
+	return len(config.Component.Decentralized) == 0 && config.Component.RSS != nil && len(config.Component.Federated) == 0
+}
+
+// CalculateWorkerCount returns the number of workers deployed
+func CalculateWorkerCount(config *File) int {
+	return len(config.Component.Decentralized) + lo.Ternary(config.Component.RSS != nil, 1, 0) + len(config.Component.Federated)
+}
+
+// CalculateComponentCount returns the number of components deployed
+func CalculateComponentCount(config *File) int {
+	return lo.Ternary(len(config.Component.Decentralized) > 0, 1, 0) + lo.Ternary(config.Component.RSS != nil, 1, 0) + lo.Ternary(len(config.Component.Federated) > 0, 1, 0)
+}
